728000f5439b03e680896ad0c08811419ef25c5f
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2021-04-10 16:15:34 +0200

    Corrected logical pixels error

--- constants.dart
+++ constants.dart
@@ -27,65 +27,68 @@ class Fed {
   static Color purple       = WidgetsBinding.instance.window.platformBrightness == Brightness.light ? Colors.purple[400] : Colors.purple[200];
   static Color red          = WidgetsBinding.instance.window.platformBrightness == Brightness.light ? Colors.red[400] : Colors.red[300];
 
+  static final deviceHeight = WidgetsBinding.instance.window.physicalSize.height / WidgetsBinding.instance.window.devicePixelRatio;
+  static final deviceWidth = WidgetsBinding.instance.window.physicalSize.width / WidgetsBinding.instance.window.devicePixelRatio;
+
   // Vertical spacings used for MARGINS and PADDINGS
-  static double v1 = 12 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double v2 = 14 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double v3 = 16 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double v4 = 18 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double v5 = 20 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double v6 = 24 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double v7 = 32 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.6 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
+  static double v1 = 12 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
+  static double v2 = 14 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
+  static double v3 = 16 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
+  static double v4 = 18 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
+  static double v5 = 20 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
+  static double v6 = 24 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
+  static double v7 = 32 / (deviceHeight < 700 ? 1.6 : deviceHeight < 800 ? 1.15 : 1);
 
   // Horizontal spacings used for MARGINS and PADDINGS
-  static double h1 = 12 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double h2 = 14 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double h3 = 16 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double h4 = 18 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double h5 = 20 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double h6 = 24 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double h7 = 32 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
+  static double h1 = 12 / (deviceWidth < 380 ? 1.3 : 1);
+  static double h2 = 14 / (deviceWidth < 380 ? 1.3 : 1);
+  static double h3 = 16 / (deviceWidth < 380 ? 1.3 : 1);
+  static double h4 = 18 / (deviceWidth < 380 ? 1.3 : 1);
+  static double h5 = 20 / (deviceWidth < 380 ? 1.3 : 1);
+  static double h6 = 24 / (deviceWidth < 380 ? 1.3 : 1);
+  static double h7 = 32 / (deviceWidth < 380 ? 1.3 : 1);
 
   // Font sizes
-  static double xxs   = 12 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double xs    = 14 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double s     = 16 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double m     = 18 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double l     = 20 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double xl    = 24 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
-  static double xxl    = 32 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.3 : 1);
+  static double xxs   = 12 / (deviceWidth < 380 ? 1.3 : 1);
+  static double xs    = 14 / (deviceWidth < 380 ? 1.3 : 1);
+  static double s     = 16 / (deviceWidth < 380 ? 1.3 : 1);
+  static double m     = 18 / (deviceWidth < 380 ? 1.3 : 1);
+  static double l     = 20 / (deviceWidth < 380 ? 1.3 : 1);
+  static double xl    = 24 / (deviceWidth < 380 ? 1.3 : 1);
+  static double xxl    = 32 / (deviceWidth < 380 ? 1.3 : 1);
 
   // Heights
-  static double a1    = 4 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a2    = 8 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a3    = 12 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a4    = 16 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a5    = 24 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a6    = 32 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a7    = 48 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a8    = 64 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a9    = 96 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a10   = 128 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a11   = 192 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a12   = 256 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a13   = 384 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
-  static double a14   = 512 / (WidgetsBinding.instance.window.physicalSize.height < 700 ? 1.3 : WidgetsBinding.instance.window.physicalSize.height < 800 ? 1.15 : 1);
+  static double a1    = 4 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a2    = 8 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a3    = 12 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a4    = 16 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a5    = 24 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a6    = 32 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a7    = 48 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a8    = 64 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a9    = 96 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a10   = 128 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a11   = 192 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a12   = 256 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a13   = 384 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
+  static double a14   = 512 / (deviceHeight < 700 ? 1.3 : deviceHeight < 800 ? 1.15 : 1);
   static double aFull = double.infinity;
 
   // Widths
-  static double w1    = 4 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w2    = 8 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w3    = 12 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w4    = 16 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w5    = 24 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w6    = 32 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w7    = 48 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w8    = 64 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w9    = 96 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w10   = 128 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w11   = 192 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w12   = 256 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w13   = 384 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
-  static double w14   = 512 / (WidgetsBinding.instance.window.physicalSize.width < 380 ? 1.5 : 1);
+  static double w1    = 4 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w2    = 8 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w3    = 12 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w4    = 16 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w5    = 24 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w6    = 32 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w7    = 48 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w8    = 64 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w9    = 96 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w10   = 128 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w11   = 192 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w12   = 256 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w13   = 384 / (deviceWidth < 380 ? 1.5 : 1);
+  static double w14   = 512 / (deviceWidth < 380 ? 1.5 : 1);
   static double wFull = double.infinity;
 
   // Factory that creates the singleton
